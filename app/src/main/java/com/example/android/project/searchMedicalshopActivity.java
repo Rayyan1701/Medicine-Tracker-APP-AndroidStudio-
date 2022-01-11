1d2b5c43126e855e3a9e0088d51e291fed1917fa
Rayyan_1701 <rayyan.is19@bmsce.ac.in>
2022-01-11 17:53:25 +0530

    90% done

--- app/src/main/java/com/example/android/project/searchMedicalshopActivity.java
+++ app/src/main/java/com/example/android/project/searchMedicalshopActivity.java
@@ -1,20 +1,106 @@
 package com.example.android.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.widget.Toast;
 
+import com.example.android.project.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class searchMedicalshopActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+
+public class searchMedicalshopActivity extends AppCompatActivity  implements OnMapReadyCallback {
     View c1,c2,c3;
+
+    private GoogleMap mMap;
+
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_medicalshop);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+
+        //loadFragment(new MapFragment());
+
+
+        searchView = findViewById(R.id.idSearchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // on below line we are getting the
+                // location name from search view.
+                String location = searchView.getQuery().toString();
+
+                // below line is to create a list of address
+                // where we will store the list of all address.
+                List<Address> addressList = null;
+
+                // checking if the entered location is null or not.
+                if (location != null || !location.equals("")) {
+                    // on below line we are creating and initializing a geo coder.
+                    Geocoder geocoder = new Geocoder(searchMedicalshopActivity.this);
+                    try {
+                        // on below line we are getting location from the
+                        // location name and adding that location to address list.
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    // on below line we are getting the location
+                    // from our list a first position.
+                    Address address;
+                    try {
+                        address = addressList.get(0);
+                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                        // on below line we are adding marker to that position.
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+
+                        // below line is to animate camera to that position.
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                    }
+                    catch (Exception e)
+                    {
+                       Toast.makeText(searchMedicalshopActivity.this,"no location as such!",Toast.LENGTH_LONG).show();
+                    }
+
+
+                    // on below line we are creating a variable for our location
+                    // where we will add our locations latitude and longitude.
+
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
 
 
         c1=(View)findViewById(R.id.today_button);
@@ -52,10 +138,32 @@ public class searchMedicalshopActivity extends AppCompatActivity {
         v.performClick();
 
 
+        mapFragment.getMapAsync(this);
 
     }
     public void myfunc(Class temp){
         Intent intent = new Intent(this, temp);
         startActivity(intent);
+    }
+
+    private void loadFragment(Fragment fragment) {
+// create a FragmentManager
+        FragmentManager fm = getFragmentManager();
+// create a FragmentTransaction to begin the transaction and replace the
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+// replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.mymapLinearlayout, fragment);
+        fragmentTransaction.commit(); // save the changes
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
